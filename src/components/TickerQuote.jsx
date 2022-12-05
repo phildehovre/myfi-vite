611c848600a33ba804a2d6bcacc8109deec1f60f
phildehovre <66724307+phildehovre@users.noreply.github.com>
2022-12-05 11:52:45 +0000

    Broken commit

--- src/components/TickerQuote.jsx
+++ src/components/TickerQuote.jsx
@@ -1,32 +1,51 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useQuote, useWatchlistByOwner } from '../utils/db'
 import Spinner from './Spinner'
 import './TickerQuote.scss'
 import './InputRange.scss'
+import { selectedTickerContext } from '../contexts/SelectedTickerProvider'
+import { useNavigate } from 'react-router-dom'
 
-function TickerQuote({ ticker }) {
+function TickerQuote() {
 
-    const [interval, setInterval] = useState('1day')
+    const navigate = useNavigate()
+
     const onSuccess = (data) => {
-        console.log(data.data)
+        if (data.code === 429) {
+            alert(data.message)
+            navigate('/')
+        }
     }
-    const { isLoading, data, error } = useQuote(ticker, interval, onSuccess)
+
+    const onError = (err) => {
+        console.log(err)
+    }
+
+    const {
+        selectedTicker,
+        interval,
+        isQuoteLoading,
+        quoteData,
+        quoteError
+    } = useContext(selectedTickerContext)
+    // const { isQuoteLoading, data, quoteError } = useQuote(selectedTicker, interval, onSuccess, onError)
+
+
+    // const quoteData = data?.data
 
 
     const renderRange = () => {
-        if (!isLoading && !error && data) {
-            const { high, low } = data.data.fifty_two_week
+        if (!isQuoteLoading && !quoteError && quoteData && quoteData.data.code !== 429) {
+            const { high, low } = quoteData.data.fifty_two_week
             return (
                 <div className='price_range-ctn'>
                     <h4>52-week range:</h4>
-                    <span>
-                        <span className='range-ctn'>
-                            {low}
-                            <input readOnly type='range' min={low} max={high} value={data.data.close} />
-                            <span className='range-percentage'></span>
-                            {high}
-                        </span>
+                    <span className='range-ctn'>
+                        {Number(low)}
+                        <input readOnly type='range' min={low} max={high} value={quoteData.data.close} />
+                        <span className='range-percentage'></span>
+                        {Number(high)}
                     </span>
                 </div>
 
@@ -38,37 +57,37 @@ function TickerQuote({ ticker }) {
 
     return (
         <>
-            {isLoading &&
+            {isQuoteLoading &&
                 <Spinner />
             }
 
-            {!isLoading && !error && data &&
+            {!isQuoteLoading && !quoteError && quoteData &&
                 <div className='quote-ctn'>
-                    <h1>{data.data.symbol}</h1>
-                    <p>{data.data.name}</p>
+                    <h1>{quoteData.data.symbol}</h1>
+                    <p>{quoteData.data.name}</p>
                     <span>
                         <h4>Open:</h4>
-                        <p>{data.data.open}</p>
+                        <p>{quoteData.data.open}</p>
                     </span>
                     <span>
                         <h4>Close:</h4>
-                        <p>{data.data.close}</p>
+                        <p>{quoteData.data.close}</p>
                     </span>
                     <span>
                         <h4>Previous Close:</h4>
-                        <p style={{ display: 'flex' }}>{data.data.previous_close}
-                            <span style={{ color: `${data.data.percent_change >= 0 ? 'lightgreen' : 'salmon'}`, fontSize: '.8em' }}>
-                                {data.data.percent_change}%
+                        <p style={{ display: 'flex' }}>{quoteData.data.previous_close}
+                            <span style={{ color: `${quoteData.data.percent_change >= 0 ? 'lightgreen' : 'salmon'}`, fontSize: '.8em' }}>
+                                {quoteData.data.percent_change}%
                             </span>
                         </p>
                     </span>
                     <span>
                         <h4>Average volume:</h4>
-                        <p>{data.data.average_volume}</p>
+                        <p>{quoteData.data.average_volume}</p>
                     </span>
                     <span>
                         <h4>Average volume:</h4>
-                        <p>{data.data.average_volume}</p>
+                        <p>{quoteData.data.average_volume}</p>
                     </span>
                     {renderRange()}
                 </div>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react({
     include: "**/*.jsx"
-  })]
+  })],
+  resolve: {
+  }
 })
 
 
